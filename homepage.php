c7c12976cc32335ea27efe302e1a71fc4443d3de
Stella Hack <foxoftheasterisk@gmail.com>
2023-11-18 03:00:38 -0800

    homepage switch if not voted recently

--- homepage.php
+++ homepage.php
@@ -6,6 +6,28 @@ if(!isset($_COOKIE['user']))
     redirect("login.php");
 }
 
+$db = connectToDB();
+
+$user = $db->real_escape_string($_COOKIE['user']);
+
+$query = "SELECT MIN(DATEDIFF(NOW(), last_voted_for)) AS days
+          FROM game_status
+          WHERE player_id='{$user}' AND last_voted_for IS NOT NULL";
+
+
+$result = $db->query($query);
+$row = $result->fetch_assoc();
+
+$needsToVote = false;
+if($row["days"] == "")
+{
+    $needsToVote = true;
+}
+else if($row["days"] >= 6)
+{
+    $needsToVote = true;
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en-US">
@@ -13,7 +35,6 @@ if(!isset($_COOKIE['user']))
         <title>Hatclub Games Voting Site</title>
         
         <?php require('header_boilerplate.html'); ?>
-        <!--TODO: redirect to login if no username cookie-->
         
     </head>
     <body>
@@ -27,12 +48,28 @@ if(!isset($_COOKIE['user']))
             <!-- TODO: show different options if not yet voted -->
             <img src="fishhat.png" alt="The Fish Hat" class="splash">
             <main class="flexcolumn">
-                <a href="gamechooser.php">
-                    <button type="button" class="big primary">Choose a game!</button>
+                <?php
+
+if($needsToVote)
+{
+    echo("      <a href='votepage.php'>
+                    <button type='button' class='big primary'>Vote!</button>
                 </a>
-                <a href="votepage.php">
-                    <button type="button" class="medium secondary">View/edit votes</button>
+                <a href='gamechooser.php'>
+                    <button type='button' class='medium secondary'>View game chooser</button>
+                </a>");
+}
+else
+{
+    echo("      <a href='gamechooser.php'>
+                    <button type='button' class='big primary'>Choose a game!</button>
                 </a>
+                <a href='votepage.php'>
+                    <button type='button' class='medium secondary'>View/edit votes</button>
+                </a>");
+}
+
+                ?>
                 <a href="gamestatus.php">
                     <button type="button" class="medium secondary">View/edit game status</button>
                 </a>
